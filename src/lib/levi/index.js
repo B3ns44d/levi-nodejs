84829ec7c4b90f5cd4fc04b532bd1a978dcea165
B3ns44d <abdsamad23bnsaad@gmail.com>
2021-12-19 16:05:53 +0100

    enhance levi

--- src/lib/levi/index.js
+++ src/lib/levi/index.js
@@ -3,6 +3,7 @@ import Route from 'route-parser'
 import { parse } from 'querystring'
 import { logger } from './utils/logger.js'
 import fs from 'fs'
+import { HTTP_METHOD } from '@shared/constants'
 
 const levi = (() => {
   let routes = []
@@ -10,6 +11,7 @@ const levi = (() => {
   const addRoute = (method, url, handler) => {
     routes.push({ method, url: new Route(url), handler })
   }
+
   const findRoute = (method, url) => {
     const route = routes.find((route) => {
       return route.method === method && route.url.match(url)
@@ -20,80 +22,79 @@ const levi = (() => {
     return { handler: route.handler, params: route.url.match(url) }
   }
 
-  const get = (route, handler) => addRoute('get', route, handler)
-  const post = (route, handler) => addRoute('post', route, handler)
-  const put = (route, handler) => addRoute('put', route, handler)
-  const omit = (route, handler) => addRoute('delete', route, handler)
+  const get = (route, handler) => addRoute(HTTP_METHOD.GET, route, handler)
+  const post = (route, handler) => addRoute(HTTP_METHOD.POST, route, handler)
+  const put = (route, handler) => addRoute(HTTP_METHOD.PUT, route, handler)
+  const omit = (route, handler) => addRoute(HTTP_METHOD.DELETE, route, handler)
 
   const router = () => {
     const listen = (port, callback) => {
-      try {
-        http
-          .createServer((req, res) => {
-            const method = req.method.toLowerCase()
-            const url = req.url.toLowerCase()
-            const route = findRoute(method, url)
+      const server = (req, res) => {
+        const method = req.method
+        const url = req.url.toLowerCase()
+        const route = findRoute(method, url)
 
-            if (route) {
-              req.params = route.params
+        if (route) {
+          req.params = route.params
 
-              res.send = (response) => {
-                if (typeof response !== 'string') {
-                  response = JSON.stringify(response)
-                }
-                res.end(response)
-                res.writeHead(200, { 'Content-Type': 'text/plain' })
-              }
-
-              res.json = (response) => {
-                res.end(JSON.stringify(response))
-                res.writeHead(200, { 'Content-Type': 'application/json' })
-              }
-
-              if (method === 'post' || method === 'put') {
-                let body = ''
-                req.on('readable', () => {
-                  let chunk = req.read()
-                  if (chunk) body += chunk.toString()
-                })
-
-                req.on('end', () => {
-                  req.body = JSON.parse(body)
-                  route.handler(req, res)
-                })
-              } else {
-                route.handler(req, res)
-              }
-            } else {
-              res.writeHead(404, { 'Content-Type': 'application/json' })
-              res.end('Route not found.')
+          res.send = (response) => {
+            if (typeof response !== 'string') {
+              response = JSON.stringify(response)
             }
+            res.end(response)
+            res.writeHead(200, { 'Content-Type': 'text/plain' })
+          }
 
-            logger(
-              `${
-                req.connection.remoteAddress
-              } - - [${new Date().toUTCString()}] "${method} ${url} HTTP/1.1" ${
-                res.statusCode
-              } ${res.getHeader('Content-Length')} "${req.headers.referer}" "${
-                req.headers['user-agent']
-              }"`
-            )
+          res.json = (response) => {
+            res.end(JSON.stringify(response))
+            res.writeHead(200, { 'Content-Type': 'application/json' })
+          }
 
-            fs.appendFile(
-              'logs/short-report.log',
-              `${
-                req.connection.remoteAddress
-              } - - [${new Date().toUTCString()}] "${method} ${url} HTTP/1.1" ${
-                res.statusCode
-              } ${res.getHeader('Content-Length')} "${req.headers.referer}" "${
-                req.headers['user-agent']
-              }"\n`,
-              function (err) {
-                if (err) throw err
-              }
-            )
-          })
-          .listen(port, callback)
+          if (method === HTTP_METHOD.POST || method === HTTP_METHOD.PUT) {
+            let body = ''
+            req.on('readable', () => {
+              let chunk = req.read()
+              if (chunk) body += chunk.toString()
+            })
+
+            req.on('end', () => {
+              req.body = JSON.parse(body)
+              route.handler(req, res)
+            })
+          } else {
+            route.handler(req, res)
+          }
+        } else {
+          res.writeHead(404, { 'Content-Type': 'application/json' })
+          res.end('Route not found.')
+        }
+
+        logger(
+          `${
+            req.connection.remoteAddress
+          } - - [${new Date().toUTCString()}] "${method} ${url} HTTP/1.1" ${
+            res.statusCode
+          } ${res.getHeader('Content-Length')} "${req.headers.referer}" "${
+            req.headers['user-agent']
+          }"`
+        )
+
+        fs.appendFile(
+          'logs/short-report.log',
+          `${
+            req.connection.remoteAddress
+          } - - [${new Date().toUTCString()}] "${method} ${url} HTTP/1.1" ${
+            res.statusCode
+          } ${res.getHeader('Content-Length')} "${req.headers.referer}" "${
+            req.headers['user-agent']
+          }"\n`,
+          function (err) {
+            if (err) throw err
+          }
+        )
+      }
+      try {
+        http.createServer(server).listen(port, callback)
       } catch (error) {
         throw new Error(error)
       }
---
+++ src/shared/constants/index.js
@@ -0,0 +1,6 @@
+export const HTTP_METHOD = {
+  GET: 'GET',
+  POST: 'POST',
+  PUT: 'PUT',
+  DELETE: 'DELETE',
+}
